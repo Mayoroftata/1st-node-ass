1953ef5a93f2bd4796a49c8552d784aa7320d1ac
Mayoroftata <objmak@gmail.com>
2025-01-21 08:13:30 +0100

    refactor: simplify express app initialization and file serving

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // console.log('Hello World');
-const app = require("express")();
-const path = require('path')
+const express = require("express");
+const app = express();
+
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(__dirname+'/index.html');
 })
 
 
